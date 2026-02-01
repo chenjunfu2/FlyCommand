a6bcdf6608937fcb9283b7af8e0c841056ceaecf
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-02-01 10:16:29 +0800

    fix

--- src/main/java/com/chenjunfu2/mixin/PlayerEntityMixin.java
+++ src/main/java/com/chenjunfu2/mixin/PlayerEntityMixin.java
@@ -5,6 +5,9 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin implements PlayerEntityMixinExtension
