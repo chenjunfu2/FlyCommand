9d72b9b4a863cfbebf608a5dd90431195d582f81
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-02-01 10:15:23 +0800

    更新

---
+++ src/main/java/com/chenjunfu2/api/PlayerEntityMixinExtension.java
@@ -0,0 +1,18 @@
+package com.chenjunfu2.api;
+
+import org.spongepowered.asm.mixin.Unique;
+
+public interface PlayerEntityMixinExtension
+{
+	@Unique
+	boolean flycommand_1_20_1$GetFlyCommandOn();
+	
+	@Unique
+	void flycommand_1_20_1$SetFlyCommandOn(boolean flyCommandOn);
+	
+	@Unique
+	boolean flycommand_1_20_1$GetLastFly();
+	
+	@Unique
+	public void flycommand_1_20_1$SetLastFly(boolean lastFly);
+}
---
+++ src/main/java/com/chenjunfu2/mixin/PlayerEntityMixin.java
@@ -0,0 +1,63 @@
+package com.chenjunfu2.mixin;
+
+import com.chenjunfu2.api.PlayerEntityMixinExtension;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.NbtCompound;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerEntityMixin implements PlayerEntityMixinExtension
+{
+	//新增字段：玩家是否开启飞行命令
+	@Unique private boolean flyCommandOn = false;
+	//新增字段：玩家刚才是否在飞行
+	@Unique private boolean lastFly = false;
+	
+	@Unique
+	@Override
+	public boolean flycommand_1_20_1$GetFlyCommandOn()
+	{
+		return flyCommandOn;
+	}
+	
+	@Unique
+	@Override
+	public void flycommand_1_20_1$SetFlyCommandOn(boolean flyCommandOn)
+	{
+		this.flyCommandOn = flyCommandOn;
+	}
+	
+	@Unique
+	@Override
+	public boolean flycommand_1_20_1$GetLastFly()
+	{
+		return lastFly;
+	}
+	
+	@Unique
+	@Override
+	public void flycommand_1_20_1$SetLastFly(boolean lastFly)
+	{
+		this.lastFly = lastFly;
+	}
+	
+	@Inject(method = "readCustomDataFromNbt", at = @At("TAIL"))
+	private void loadFlyCommandData(NbtCompound nbt, CallbackInfo ci)
+	{
+		NbtCompound modCompoundData = nbt.getCompound("FlyCommandModData");
+		
+		this.flyCommandOn = modCompoundData.getBoolean("flyCommandOn");
+		this.lastFly = modCompoundData.getBoolean("lastFly");
+	}
+	
+	@Inject(method = "writeCustomDataToNbt", at = @At("TAIL"))
+	private void saveFlyCommandData(NbtCompound nbt, CallbackInfo ci)
+	{
+		NbtCompound modCompoundData = new NbtCompound();
+		modCompoundData.putBoolean("flyCommandOn", flyCommandOn);
+		modCompoundData.putBoolean("lastFly", lastFly);
+		
+		nbt.put("FlyCommandModData", modCompoundData);
+	}
+}
