7143671d2e6692a84840430c1c3b76bb5ccf841d
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-02-25 08:07:39 +0800

    客户端再次修正

--- src/main/java/com/chenjunfu2/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/com/chenjunfu2/mixin/ClientPlayNetworkHandlerMixin.java
@@ -1,17 +1,16 @@
 package com.chenjunfu2.mixin;
 
-
-import com.chenjunfu2.api.ClientPlayerEntityMixinExtension;
 import com.chenjunfu2.api.PlayerEntityMixinExtension;
-import com.llamalad7.mixinextras.sugar.Local;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
 import net.minecraft.network.packet.s2c.play.PlayerAbilitiesS2CPacket;
 import net.minecraft.world.GameMode;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -20,15 +19,41 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientPlayNetworkHandler.class)
 public abstract class ClientPlayNetworkHandlerMixin
 {
-	@Inject(method = "onPlayerAbilities",at = @At(value = "RETURN"))
-	void onPlayerAbilitiesInject(PlayerAbilitiesS2CPacket packet, CallbackInfo ci, @Local(index = 2) PlayerEntity playerEntity)
+	@Shadow @Final private MinecraftClient client;
+	
+	@Inject(method = "onPlayerAbilities", at = @At(value = "RETURN"))
+	void onPlayerAbilitiesInject(PlayerAbilitiesS2CPacket packet, CallbackInfo ci)
 	{
-		if(!(playerEntity instanceof ClientPlayerEntity))
+		if(client.player == null || client.interactionManager == null)
 		{
 			return;
 		}
 		
-		GameMode clientGameMode = ((ClientPlayerEntityMixinExtension)playerEntity).flycommand_1_20_1$GetGameMode();
-		((PlayerEntityMixinExtension)playerEntity).flycommand_1_20_1$SetFlyCommandOn(clientGameMode.isSurvivalLike() && playerEntity.getAbilities().allowFlying);
+		GameMode clientGameMode = client.interactionManager.getCurrentGameMode();
+		((PlayerEntityMixinExtension)client.player).flycommand_1_20_1$SetFlyCommandOn(clientGameMode.isSurvivalLike() && client.player.getAbilities().allowFlying);
+		
+		if(client.player.getAbilities().flying)
+		{
+			((PlayerEntityMixinExtension)client.player).flycommand_1_20_1$SetLastFly(true);
+		}
 	}
+	
+	@Inject(method = "onGameJoin", at = @At(value = "RETURN"))
+	void onGameJoinInject(GameJoinS2CPacket packet, CallbackInfo ci)
+	{
+		if(client.player == null || client.interactionManager == null)
+		{
+			return;
+		}
+		
+		GameMode clientGameMode = client.interactionManager.getCurrentGameMode();
+		((PlayerEntityMixinExtension)client.player).flycommand_1_20_1$SetFlyCommandOn(clientGameMode.isSurvivalLike() && client.player.getAbilities().allowFlying);
+		
+		if(client.player.getAbilities().flying)
+		{
+			((PlayerEntityMixinExtension)client.player).flycommand_1_20_1$SetLastFly(true);
+		}
+		
+	}
+	
 }
--- src/main/java/com/chenjunfu2/mixin/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/com/chenjunfu2/mixin/ServerPlayerInteractionManagerMixin.java
@@ -11,7 +11,6 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import net.minecraft.world.GameMode;
 import net.minecraft.server.network.ServerPlayerInteractionManager;
 
-
 @Mixin(ServerPlayerInteractionManager.class)
 abstract class ServerPlayerInteractionManagerMixin
 {
